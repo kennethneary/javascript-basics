ad47e06cf732f7f5d538d639d3107b7d84d9dfe8
Kenneth Neary <kennethneary@gmail.com>
2020-05-14 20:56:48 +0100

    add try catch basics

---
+++ try-catch/try.js
@@ -0,0 +1,46 @@
+function testError() {
+    try {
+        throw new Error("error"); // this is executed
+    } catch(error) {
+        console.log("catch", error);
+        return 1;
+    } finally {
+        console.log("finally");
+        return 1000;
+    }
+}
+console.log(testError()); // finally 1000
+
+
+function asyncJob(result, timeMilliseconds, fail) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if (fail) {
+                reject(result); 
+            } else {
+                resolve(result);
+            }
+        }, timeMilliseconds);
+    });
+}
+async function run(isFail) {
+    try {
+        return await asyncJob(1, 2000, isFail)
+    } catch (error) {
+        console.log("catch", error);
+        return 'catch recovered';
+    }
+}
+run(true).then(console.log);
+run(false).catch(console.log);
+
+
+async function runReThrow(isFail) {
+    try {
+        return await asyncJob('re throw error after logging', 2000, isFail)
+    } catch (error) {
+        console.log("catch", error);
+        throw error; // re throw error after logging
+    }
+}
+runReThrow(true).catch(console.log);
